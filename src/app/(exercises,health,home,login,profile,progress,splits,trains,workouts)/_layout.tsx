ce787746a7c71992682210cf89e44d4f5e52668e
Tariq Horan <N00202437@iadt.ie>
2023-12-10 16:51:09 +0000

    Add App component and update navigation

--- src/app/(exercises,health,home,login,profile,progress,splits,trains,workouts)/_layout.tsx
+++ src/app/(exercises,health,home,login,profile,progress,splits,trains,workouts)/_layout.tsx
@@ -1,17 +1,117 @@
-import { Slot, Redirect } from "expo-router";
-
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import Exercise from "./exercises/Index";
+import Health from "./health/Index";
+import Home from "./home/Index";
+import Login from "./login/Index";
+import Profile from "./profile/Index";
+import Progress from "./progress/Index";
+import Split from "./splits/Index";
+import Train from "./trains/Index";
+import Workout from "./workouts/Index";
 import { useSession } from "../../contexts/AuthContext";
+import { PaperProvider } from "react-native-paper";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Text } from "react-native";
 
-export default function AuthLayout() {
-  const { session } = useSession();
+export default function Nav() {
+  const { session }: any = useSession();
+  const hiddenScreens = {
+    Exercise,
+    Workout,
+    Split,
+    Train,
+    // login,
+  };
 
-  if (!session) {
-    return <Redirect href="/" />;
-  }
+  const Tab = createBottomTabNavigator();
+  const Stack = createStackNavigator();
+
+  // Generic function to create a hidden stack navigator for a specific screen
+  const createHiddenStackNavigator = (screenName: any, component: any) => (
+    <Stack.Navigator>
+      <Stack.Screen name={screenName} component={component} />
+    </Stack.Navigator>
+  );
 
   return (
-    <>
-      <Slot />
-    </>
+    <Tab.Navigator
+      initialRouteName={session ? "(home)" : "(login)"}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          // Set icon based on the route name
+          if (route.name === "(home)") {
+            iconName = "home";
+          } else if (route.name === "(progress)") {
+            iconName = "trending-up";
+          } else if (route.name === "(health)") {
+            iconName = "favorite";
+          } else if (route.name === "(profile)") {
+            iconName = "person";
+          }
+
+          // Return the icon component
+          return (
+            <MaterialIcons name={iconName as any} size={size} color={color} />
+          );
+        },
+        tabBarLabel: ({ focused, color }) => {
+          const label = route.name.replace(/\(([^)]+)\)/, "$1"); // Remove parentheses but keep the content inside
+          return <Text style={{ color, fontSize: 12 }}>{label}</Text>;
+        },
+      })}>
+      <Tab.Screen
+        name="(home)"
+        component={Home}
+        options={{ headerTitle: "Home" }}
+      />
+      <Tab.Screen
+        name="(progress)"
+        component={Progress}
+        options={{
+          headerTitle: "Progress",
+        }}
+      />
+      <Tab.Screen
+        name="(health)"
+        component={Health}
+        options={{
+          headerTitle: "Health",
+        }}
+      />
+      <Tab.Screen
+        name="(profile)"
+        component={Profile}
+        options={{
+          headerTitle: "Profile",
+        }}
+      />
+
+      <Tab.Screen
+        name="(login)"
+        component={Login}
+        options={{
+          tabBarButton: () => null,
+          tabBarStyle: { display: "none" },
+          headerShown: false,
+        }}
+      />
+
+      {Object.entries(hiddenScreens).map(([screenName, component]) => (
+        <Tab.Screen
+          key={screenName}
+          name={`(${screenName.toLowerCase()}s)`}
+          options={{
+            tabBarButton: () => null,
+            tabBarStyle: false,
+            headerShown: false,
+          }}>
+          {() => createHiddenStackNavigator(screenName, component)}
+        </Tab.Screen>
+      ))}
+    </Tab.Navigator>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,18 @@
+// App.js
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { SessionProvider } from "../contexts/AuthContext";
+import { SessionProvider, useSession } from "../contexts/AuthContext";
 import { PaperProvider } from "react-native-paper";
-import Nav from "./_layout";
 import AuthLayout from "./(exercises,health,home,login,profile,progress,splits,trains,workouts)/_layout";
+import LoginPage from "./(exercises,health,home,login,profile,progress,splits,trains,workouts)/login/Index";
+import Nav from "./(exercises,health,home,login,profile,progress,splits,trains,workouts)/_layout";
 
 export default function App() {
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <SessionProvider>
-          <Nav />
-          <AuthLayout />
-        </SessionProvider>
-      </NavigationContainer>
-    </PaperProvider>
+    <SessionProvider>
+      <PaperProvider>
+        <Nav />
+      </PaperProvider>
+    </SessionProvider>
   );
 }
